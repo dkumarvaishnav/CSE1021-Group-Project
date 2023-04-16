77ea6b6dd5f9fc919329734e1a3a8e49cd7df6b6
Kumarvaishnav <dkumarvaishnav47@gmail.com>
2023-04-16 21:14:37 +0530

    Add files via upload

---
+++ Question 9.py
@@ -0,0 +1,13 @@
+#start of the program
+#take inputfrom the user
+P1 = float(input("Enter P1: "))
+V1 = float(input("Enter V1: "))
+P2 = float(input("Enter P2: "))
+
+#calculating the required V2 using formula
+V2 = V1 * (P1 / P2)
+
+#display the result
+print("V2 =", V2)
+
+#end of program
